02d43310c17fbfb2a123eabf900b85f83e908dcc
Andrey A <fromjfktosfo@gmail.com>
2023-07-15 13:21:06 -0400

    design polish

--- app/src/main/java/com/example/mycalculator/MainActivity.java
+++ app/src/main/java/com/example/mycalculator/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.mycalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,87 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void buttonZeroOnCLick(View view)
+    {}
+    public void buttonOneOnCLick(View view)
+    {}
+    public void buttonTwoOnCLick(View view)
+    {}
+    public void buttonThreeOnCLick(View view)
+    {}
+    public void buttonFourOnCLick(View view)
+    {}
+    public void buttonFiveOnCLick(View view)
+    {}
+    public void buttonSixOnCLick(View view)
+    {}
+    public void buttonSevenOnCLick(View view)
+    {}
+    public void buttonEightOnCLick(View view)
+    {}
+    public void buttonNineOnCLick(View view)
+    {}
+
+// -----------------
+
+    public void buttonClearOnClick(View view)
+    {
+
+    }
+
+    public void buttonParenthesisOnClick(View view)
+    {
+
+    }
+
+    public void buttonExponentOnClick(View view)
+    {
+
+    }
+
+    public void buttonDivideOnClick(View view)
+    {
+
+    }
+
+    public void buttonMultiplyOnClick(View view)
+    {
+
+    }
+
+
+    public void buttonSubstractOnClick(View view)
+    {
+
+    }
+
+
+    public void buttonAddOnClick(View view)
+    {
+
+    }
+
+
+    public void buttonPlusMinusOnClick(View view)
+    {
+
+    }
+
+
+    public void buttonPointOnClick(View view)
+    {
+
+    }
+
+    public void buttonEqualsOnClick(View view)
+    {
+
+    }
+
+    public void buttonDeleteOnClick(View view)
+    {
+
     }
 }
